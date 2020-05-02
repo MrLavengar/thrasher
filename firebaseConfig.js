57c1f436d3ee7709085cc85649311133a6d5f1c3
Mesiya391 <bjasinski391@gmail.com>
2020-05-02 19:20:32 +0200

    fix2

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,20 +1,16 @@
 /*src="https://www.gstatic.com/firebasejs/7.14.2/firebase-app.js"
 src="https://www.gstatic.com/firebasejs/7.14.2/firebase-analytics.js"
 src="https://www.gstatic.com/firebasejs/7.14.2/firebase-auth.js"
-src="https://www.gstatic.com/firebasejs/7.14.2/firebase-firestore.js" 
-*/
+src="https://www.gstatic.com/firebasejs/7.14.2/firebase-firestore.js" */
 
 var firebaseConfig = {
-    apiKey: "AIzaSyCiJ3XXznA9PyIJ49v1zGdJEFoV8skHqY8",
-    authDomain: "trasher-92594.firebaseapp.com",
-    databaseURL: "https://trasher-92594.firebaseio.com",
-    projectId: "trasher-92594",
-    storageBucket: "trasher-92594.appspot.com",
-    messagingSenderId: "327023580762",
-    appId: "1:327023580762:web:4b6ad0748288a5fa2baa63"
-  };
-  firebase.initializeApp(firebaseConfig);
-  const auth = firebase.auth(); 
-
-
-  
+  apiKey: "AIzaSyCiJ3XXznA9PyIJ49v1zGdJEFoV8skHqY8",
+  authDomain: "trasher-92594.firebaseapp.com",
+  databaseURL: "https://trasher-92594.firebaseio.com",
+  projectId: "trasher-92594",
+  storageBucket: "trasher-92594.appspot.com",
+  messagingSenderId: "327023580762",
+  appId: "1:327023580762:web:4b6ad0748288a5fa2baa63"
+};
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth(); 
