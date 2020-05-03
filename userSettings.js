ad7ace083a1564c5481876868fe8d37d1e81660d
Mesiya391 <bjasinski391@gmail.com>
2020-05-03 09:58:21 +0200

    fix logout

--- userSettings.js
+++ userSettings.js
@@ -1,1 +1,12 @@
-$('.expand-menu .nav-link').toggleClass('animate-show');
+$(document).ready(function(){
+    
+    $('.menu').on('click', function() {
+		$('.bar').toggleClass('animate');
+        $('.expand-menu').toggleClass('animate');
+        $('.expand-menu .nav-link').toggleClass('animate');
+        setTimeout(function(){
+            $('.expand-menu .nav-link').toggleClass('animate-show');
+        },500)
+	})
+    
+})
